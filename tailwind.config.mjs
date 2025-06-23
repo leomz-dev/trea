58b778a88c5dbf8fabc1cbd5272c854764d431fc
leonardo.meza.v18 <leonardo.meza.v18@gmail.com>
2025-06-23 19:32:01 +0000

    style changing, aos on

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: [ 'sans-serif', 'montserrat'],
         display: ['Space Grotesk', 'sans-serif'],
       },
       colors: {
